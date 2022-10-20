b14bba7af959d7a82d5f7b63556b7055344bb9af
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-20 20:54:59 +0300

    Add files via upload

---
+++ 03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfNumbers = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            for (int i = 1; i <= countOfNumbers; i++)
+            {
+                decimal inputNum = decimal.Parse(Console.ReadLine());
+                sum += inputNum;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
