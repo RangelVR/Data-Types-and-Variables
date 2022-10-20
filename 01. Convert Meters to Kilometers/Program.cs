0b1d8bf2f58ae74df23d9f6ef382f5d7ea74ebba
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-20 20:24:17 +0300

    Update Program.cs

--- 01. Convert Meters to Kilometers/Program.cs
+++ 01. Convert Meters to Kilometers/Program.cs
@@ -1,14 +1,13 @@
-﻿using System;
+using System;
 
-namespace ConvertMetersToKilometers
+namespace demo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int meters = int.Parse(Console.ReadLine());
-            decimal km = 0;
-            km = meters / 1000m;
+            int metters = int.Parse(Console.ReadLine());
+            double km = (double)(metters / 1000d);
             Console.WriteLine($"{km:f2}");
         }
     }
