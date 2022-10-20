6ab076e0fab19ea3b1aad014e49d0918b495c7fc
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-20 20:33:30 +0300

    Add files via upload

---
+++ 02. Pounds to Dollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PoundsToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double britishPounds = double.Parse(Console.ReadLine());
+            decimal usDollars = (decimal)britishPounds * 1.31m;
+            Console.WriteLine($"{usDollars:f3}");
+        }
+    }
+}
