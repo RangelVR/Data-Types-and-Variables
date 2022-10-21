efbb9674cc0859151af8b9280e4c6908f02c42f3
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-21 22:08:00 +0300

    Add files via upload

---
+++ 12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                int currentNum = i;
+                int sum = 0;
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i = i / 10;
+                }
+                bool isSpecialNum = false;
+                isSpecialNum = sum == 5 || sum == 7 || sum == 11;
+                Console.WriteLine($"{currentNum} -> {isSpecialNum}");
+                sum = 0;
+                i = currentNum;
+            }
+
+        }
+    }
+}
