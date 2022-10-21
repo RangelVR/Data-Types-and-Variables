ac2d8d4436753ca92e6bb543317305a936f2a1bc
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-21 21:25:39 +0300

    Add files via upload

---
+++ 10. Lower or Upper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LowerOrUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char leter = char.Parse(Console.ReadLine());
+            if (leter >= 'a' && leter <= 'z')
+            {
+                Console.WriteLine("lower-case");
+            }
+            else if (leter >= 'A' && leter <= 'Z')
+            {
+                Console.WriteLine("upper-case");
+            }
+
+        }
+    }
+}
