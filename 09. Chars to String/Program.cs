95c56bf1fd293d1bebd6860a0522fc7075835bdd
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-21 21:18:37 +0300

    Update Program.cs

--- 09. Chars to String/Program.cs
+++ 09. Chars to String/Program.cs
@@ -12,6 +12,14 @@ namespace CharsToString
 
             Console.WriteLine($"{first}{second}{third}");
             
+            //char ch1 = char.Parse(Console.ReadLine());
+            //char ch2 = char.Parse(Console.ReadLine());
+            //char ch3 = char.Parse(Console.ReadLine());
+
+            //string word = string.Empty;
+
+            //Console.WriteLine(word + ch1 + ch2 + ch3);
+            
         }
     }
 }
