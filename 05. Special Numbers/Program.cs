2bac996a6d1b2a81480c5d2ef49f7a639568aeb8
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-10-21 20:41:05 +0300

    Add files via upload

---
+++ 05. Special Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= inputNum; i++)
+            {
+                int numberToProces = i;
+                int sum = 0;
+                while (numberToProces != 0)
+                {
+                    int currentDigit = numberToProces % 10;
+                    numberToProces = numberToProces / 10;
+                    sum += currentDigit;
+                }
+                bool isSpecial= sum == 5 || sum == 7 || sum == 11;
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
